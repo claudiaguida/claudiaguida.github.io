cc5bd58f71de8a508b124fa4b7a94803fefa8b24
Paul Mardling <mardlingp@gmail.com>
2018-01-14 16:21:08 +0000

    build mindful 21 page

--- src/components/pages/Mindful.js
+++ src/components/pages/Mindful.js
@@ -6,33 +6,33 @@ const Mindful = ()=> {
     <div id="project__content">
       <Row
         className="project__header"
-        style={{background: 'rgb(240, 220, 220)'}}>
+        style={{background: 'rgb(149, 192, 206)'}}>
         <Col sm={6} className="project_header__text">
           <h2>Mindful</h2>
           <h4>Nov 2017</h4>
         </Col>
         <Col sm={6} className="d-flex align-items-center">
-          <img src={'src/assets/images/lesalon-lightpink.png'} />
+          <img src={'src/assets/images/mindful.png'} />
         </Col>
       </Row>
       <Row className="project__overview">
         <Col sm={12}>
           <h3>Project Overview</h3>
-          <h5>My Role</h5>
-          <p>As part of a team of 3 UX designers, we had 2-week and a half Agile sprint to redesign Mindful website using the double diamond method and the main UX design tools. I led the Discovery and Delivery phases of the Design process. My main strengths include Competitive Analysis, User Interviews, Affinity Maps, Personas, Wireframing and Design Ideation.</p>
-          <h5>The Client</h5>
-          <p>Mindful is the leading on-demand beauty service in London. Offering exclusive experience at home, they want their clients to feel empowered and looked after. They strive to offer services that are convenient, affordable and high quality.</p>
+          {/* <h5>My Role</h5> */}
+          <p>During my first week as UX immersive student at GA, I got asked to create an app for one of my classmate, Jon. Mindful 21 was created to help Jon to get into a meditation routine and to build a new long-lasting habit.</p>
+          {/* <h5>The Client</h5> */}
+          {/* <p>Mindful is the leading on-demand beauty service in London. Offering exclusive experience at home, they want their clients to feel empowered and looked after. They strive to offer services that are convenient, affordable and high quality.</p> */}
           <h5>Brief</h5>
           <p>To redesign Mindful website with a human-centred approach, with the goal to reduce the landing page bounce rate and improve the conversion.</p>
           <h5>Problem</h5>
-          <p>Thanks to an extensive research and a good understanding of our persona and user journey, we understood the main problems laid in the Booking process and in the Navigation. We also had the opportunity to test and improve other critical features such us the Services, the Treatments and the Date&Time pages.</p>
+          <p>Jon is a busy editor who has recently experienced an injury and has partially lost his ears. His daily routine has dramatically changed since then, so he wants to regain his tranquillity by using a meditation app. Jon has tried already other meditation apps but he finds them extremely complicated and overwhelming. He wants a product which is easy to use and customisable according to his busy schedule.</p>
           <h5>Solution</h5>
-          <p>After many design iterations and testings, we brought our product from low to high fidelity implementing each time the feedback received. We implemented a brand new Basket, that also served as booking summary, so users can have full control over their purchases. We also introduced a Progress bar that helped users to know what to expect at each stage of their booking.</p>
+          <p>After an extensive research and spending lots of time with my user testing different versions of the paper prototypes, I created a very simple meditation App that Jon would enjoy to use. The user starts experiencing a sense of calm already by opening the landing page. Here he will be welcomed with a free 2 minutes meditation. There is no signup form, he can decide to log-in to the App after this meditation. This welcoming feeling will last throughout the entire experience. The user, after signing up, would land on the setting page and customise the duration time of the podcast and also set a reminder linked to his personal calendar.</p>
           <h5>Outcome</h5>
-          <p>The design delivered was really well received, Mindful stakeholders felt so strong about the solutions suggested that have already implemented the majority of it. See the Mindful website here <a href="https://lesalon.com" rel="noopener  noreferrer" target="_blank" >https:&#47;&#47;lesalon.com</a></p>
+          <p>Jon was extremely happy with the result and he said he would definitely use Mindful 21. I also gathered great feedback on the visual aspect of it. Most of the users found it very pleasing to the eye and felt a sense of relaxation while interacting with it.</p>
           <h5>Tools</h5>
-          <p>Paper & sharpies, Sketch, Invision, Trello.</p>
-          <a href="http://bit.ly/2CIsV7W" rel="noopener noreferrer" target="_blank">Check out the case study...</a>
+          <p>Lots of sharpies and Marvel App.</p>
+          <a href="http://bit.ly/2D3Mh7F" rel="noopener noreferrer" target="_blank">Check out the case study...</a>
         </Col>
       </Row>
     </div>
