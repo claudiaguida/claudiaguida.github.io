1b7b8d0e8d6a94ec20f0c91b2ad0aec19959efac
Paul Mardling <mardlingp@gmail.com>
2018-01-14 16:43:52 +0000

    style contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,10 +6,7 @@ class Contact extends React.Component {
     return (
       <div id="contact">
         <Row className="contact__heading">
-          <h2>Let&apos;s Talk</h2>
-          <p>
-          Feel free to reachout if you want to collaborate, connect or just have a chat
-          </p>
+          <h2>LET&apos;S TALK</h2>
         </Row>
         {/*<Row className="contact-mobile">
           <a href="tel:+447943227683"><i className="fa fa-mobile" aria-hidden="true"></i></a>
@@ -20,9 +17,14 @@ class Contact extends React.Component {
           <a href="mailto:claudia.guida4">claudia.guida4</a>
         </Row>
         <Row className="contact__twitter">
-          <i className="fa fa-twitter" aria-hidden="true"></i>
+          <a href="https://twitter.com/CloguiGuida">
+            <i className="fa fa-twitter" aria-hidden="true"></i>
+          </a>
           <a href="https://twitter.com/CloguiGuida">Twitter</a>
         </Row>
+        <p>
+          Feel free to reachout if you want to collaborate, connect or just have a chat
+        </p>
       </div>
 
     );
