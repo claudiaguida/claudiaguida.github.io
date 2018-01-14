e5b8429761fe2b27d08f52081e4b6dfd73495712
Paul Mardling <mardlingp@gmail.com>
2018-01-14 13:19:51 +0000

    add name to navbar

--- src/components/common/MainNav.js
+++ src/components/common/MainNav.js
@@ -29,8 +29,9 @@ export default class MainNav extends React.Component {
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <Nav className="ml-auto">
+        <div className="collapse navbar-collapse navigation" id="navbarSupportedContent">
+          <h4>Claudia Guida</h4>
+          <Nav className="">
             <NavItem>
               <NavLink href="/projects">Projects</NavLink>
             </NavItem>
